2fe4bd06a2e4643f55b50b73703f76d80be8acfc
dklickman <dklickman@gmail.com>
2017-12-18 20:38:27 -0600

    edit file

--- roman_to_int.py
+++ roman_to_int.py
@@ -1,20 +1,3 @@
-'''
-l1 = list()
-for i in range(0,10):
-    l1.append(i)
-print(sum(l1))
-
-nums = [1,1,2]
-class Solution:
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        return (len(set(nums))
-print(Solution().removeDuplicates(nums))
-'''
-
 class Solution:
     def romanToInt(self, s):
         """
